3e784d5f6960e79ed483e3d97612f6443013c0eb
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-03-28 21:27:48 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => console.log("MongoDB connection established!"));
